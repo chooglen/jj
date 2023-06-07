43903a41a00622252268ebed5b5bf55f35806312
Glen Choo <chooglen@google.com>
2023-06-06 20:32:02 -0700

    WIP create working submodule clone

--- tests/test_git_submodule.rs
+++ tests/test_git_submodule.rs
@@ -63,3 +63,56 @@ fn test_gitsubmodule_print_gitmodules() {
 	path:new
     "###);
 }
+
+#[test]
+fn test_gitsubmodule_clone() {
+    let test_env = TestEnvironment::default();
+    let super_workspace_root = test_env.env_root().join("repo");
+    git2::Repository::init(&super_workspace_root).unwrap();
+    test_env.jj_cmd_success(&super_workspace_root, &["init", "--git-repo", "."]);
+
+    std::fs::write(
+        super_workspace_root.join(".gitmodules"),
+        "
+[submodule \"with/slashes/\"]
+	path = foo
+	url = ../submodule
+",
+    )
+    .unwrap();
+
+    // Set up a repo to clone the submodule from
+    let sub_git_path = test_env.env_root().join("submodule");
+    git2::Repository::init(&sub_git_path).unwrap();
+    let sub_git_repo = git2::Repository::init(sub_git_path).unwrap();
+    let signature =
+        git2::Signature::new("Some One", "some.one@example.com", &git2::Time::new(0, 0)).unwrap();
+    let mut tree_builder = sub_git_repo.treebuilder(None).unwrap();
+    let file_oid = sub_git_repo.blob(b"content").unwrap();
+    tree_builder
+        .insert("file", file_oid, git2::FileMode::Blob.into())
+        .unwrap();
+    let tree_oid = tree_builder.write().unwrap();
+    let tree = sub_git_repo.find_tree(tree_oid).unwrap();
+    sub_git_repo
+        .commit(
+            Some("refs/heads/main"),
+            &signature,
+            &signature,
+            "message",
+            &tree,
+            &[],
+        )
+        .unwrap();
+    sub_git_repo.set_head("refs/heads/main").unwrap();
+
+    // Actually clone submodule
+    let stdout = test_env.jj_cmd_success(
+        &super_workspace_root,
+        &["git", "submodule", "clone", "with/slashes/"],
+    );
+    insta::assert_snapshot!(stdout, @r###"
+    Cloned submodule "with/slashes/"
+    Nothing changed.
+    "###);
+}
